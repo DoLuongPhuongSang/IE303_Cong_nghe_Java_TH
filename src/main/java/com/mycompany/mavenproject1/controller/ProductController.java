c4f42b6b5de1a83ac97184f76f1d38065367fb5a
Do Luong Phuong Sang <22521237@gm.uit.edu.vn>
2025-06-19 21:43:31 +0700

    Update btth4_backend

--- src/main/java/com/mycompany/mavenproject1/controller/ProductController.java
+++ src/main/java/com/mycompany/mavenproject1/controller/ProductController.java
@@ -1,15 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.mycompany.mavenproject1.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.mavenproject1.model.Product;
 import com.mycompany.mavenproject1.repository.ProductRepository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
